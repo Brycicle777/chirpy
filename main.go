377446e1138a434bbc9e1772437386fa26344b06
Bryce Hargis <brycehargis@outlook.com>
2025-02-13 13:06:26 -0800

    add readiness handler and logo

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ import (
 	"net/http"
 )
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.Handle("/healthz", http.HandlerFunc(handlerReadiness))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
