cb884087f893032efe90558f05ad32a8f30ffe14
Bryce Hargis <brycehargis@outlook.com>
2025-02-13 14:30:11 -0800

    track metrics

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -10,13 +23,32 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %v", hits)))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Hits reset."))
+}
+
 func main() {
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serveMux.Handle("/healthz", http.HandlerFunc(handlerReadiness))
+	mux := http.NewServeMux()
+	apiCfg := apiConfig{}
+	handlerApp := http.FileServer(http.Dir("."))
+	handlerApp = http.StripPrefix("/app", handlerApp)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handlerApp))
+	mux.Handle("/healthz", http.HandlerFunc(handlerReadiness))
+	mux.Handle("/metrics", http.HandlerFunc(apiCfg.handlerMetrics))
+	mux.Handle("/reset", http.HandlerFunc(apiCfg.handlerReset))
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: serveMux,
+		Handler: mux,
 	}
 	server.ListenAndServe()
 }
